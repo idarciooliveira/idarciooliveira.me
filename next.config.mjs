15429d55130ffa3f1c8f8696892c9050cbdd52c6
idarciooliveira <idarciooliveira@gmail.com>
2024-03-12 23:59:02 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
     output: 'export',
+
+    /**
+     * Tell Next.js where the `public` folder is.
+     * Replace `nextjs-github-pages` with your Github repo project name.
+     */
+    assetPrefix: isProd ? "/idarciooliveira.me/" : "",
+    images: {
+        unoptimized: true,
+    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
     },
 };
-
-export default nextConfig;
